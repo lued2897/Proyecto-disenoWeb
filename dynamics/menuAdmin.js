c94bb0f1c420f4e1282d8ad155e50d3e19939641
Francisco-Galindo <paqui10718@gmail.com>
2023-04-16 20:32:21 -0600

    Activando modales desde JS

---
+++ dynamics/menuAdmin.js
@@ -0,0 +1,12 @@
+const loginModal = new bootstrap.Modal('#sesionAdmin', {})
+const menuAdminModal = new bootstrap.Modal('#menuAdmin', {})
+const listaClientesModal = new bootstrap.Modal('#listaCliente', {})
+const modificarProductoModal = new bootstrap.Modal('#modificarProducto', {})
+const datoModificadoModal = new bootstrap.Modal('#datoModificado', {})
+const inventarioModal = new bootstrap.Modal('#mostrarInventario', {})
+const crearClienteModal = new bootstrap.Modal('#crearCliente', {})
+
+const modProductosBoton = document.getElementById('modificar-productos-button');
+const listaUsuariosBoton = document.getElementById('lista-usuarios-button');
+const inventarioBoton = document.getElementById('inventario-button');
+
